cbe08dcb7c642721aad8b3986ecc9d61a10658f1
Matmozaur <matt950806@wp.pl>
2023-07-11 22:11:38 +0200

    Add scala fixes

--- Simple_API/akka/app/Main.scala
+++ Simple_API/akka/app/Main.scala
@@ -2,13 +2,32 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 import akka.stream.ActorMaterializer
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 import scala.io.StdIn
+
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
+case class SimpleData(name: String)
 
 object Main extends App {
   implicit val system = ActorSystem("my-system")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
+  implicit val simpleDataFormat = jsonFormat1(SimpleData)
+
+  implicit val simpleDataUnmarshaller: FromEntityUnmarshaller[SimpleData] =
+    Unmarshaller.byteStringUnmarshaller
+      .mapWithCharset { (data, charset) =>
+        val string = data.decodeString(charset.nioCharset.name)
+        string.parseJson.convertTo[SimpleData]
+      }
 
   val routes =
     path("simple_read") {
@@ -18,8 +37,8 @@ object Main extends App {
     } ~
     path("simple_write") {
       post {
-        entity(as[Map[String, String]]) { data =>
-          if (data.get("name").contains("x")) {
+        entity(as[SimpleData]) { data =>
+          if (data.name == "x") {
             complete(HttpEntity(ContentTypes.`application/json`, """{"message": "y"}"""))
           } else {
             complete(HttpEntity(ContentTypes.`application/json`, """{"message": "n"}"""))
@@ -34,8 +53,8 @@ object Main extends App {
     } ~
     path("simple_write_sync") {
       post {
-        entity(as[Map[String, String]]) { data =>
-          if (data.get("name").contains("x")) {
+        entity(as[SimpleData]) { data =>
+          if (data.name == "x") {
             complete(HttpEntity(ContentTypes.`application/json`, """{"message": "y"}"""))
           } else {
             complete(HttpEntity(ContentTypes.`application/json`, """{"message": "n"}"""))
@@ -52,4 +71,8 @@ object Main extends App {
   bindingFuture
     .flatMap(_.unbind())
     .onComplete(_ => system.terminate())
+      // Keep the application running
+  while (true) {
+    Thread.sleep(1000)
+  }
 }
---
+++ Simple_API/akka/app/build.sbt
@@ -0,0 +1,4 @@
+libraryDependencies += "com.typesafe.akka" %% "akka-http" % "10.2.6"
+libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.6.16"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.6.16"
+libraryDependencies += "io.spray" %% "spray-json" % "1.3.6"
