bbd2eb3185024d7fa4322bf2bf7b69751c7e5616
Matmozaur <matt950806@wp.pl>
2023-07-18 20:52:56 +0200

    Parametrize wrk script

--- Simple_REST_API/comparison/script_wrk.py
+++ Simple_REST_API/comparison/script_wrk.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import subprocess
 import time
 import requests
@@ -8,6 +9,11 @@ log.setLevel(logging.INFO)
 
 containers = [('fast-api', 8081), ('flask', 8082), ('fiber', 8083), ('gin', 8084), ('akka', 8085)]
 get_endpoints = ['simple_read']
+post_endpoints = ['simple_write']
+
+WRK_TIME = os.getenv('WRK_TIME')
+WRK_CONNECTIONS = os.getenv('WRK_CONNECTIONS')
+WRK_THREADS = os.getenv('WRK_THREADS')
 
 
 if __name__ == '__main__':
@@ -26,11 +32,20 @@ if __name__ == '__main__':
                 logging.info(f'Service {freamework} not yet ready')
                 time.sleep(5)
 
-    for endpoint in get_endpoints:
-        subprocess.run([f'echo "{endpoint}" >> /app/results/results.txt'], shell=True)
-        for freamework, port in containers:
-            subprocess.run([f'echo "{freamework}" >> /app/results/results.txt'], shell=True)
-            subprocess.run([f'wrk -t4 -c50 -d10s http://{freamework}:{port}/{endpoint} >> /app/results/results.txt'],
+    for freamework, port in containers:
+        subprocess.run([f'echo "{freamework}" >> /app/results/results.txt'], shell=True)
+        for endpoint in get_endpoints:
+            subprocess.run([f'echo "{endpoint}" >> /app/results/results.txt'], shell=True)
+            subprocess.run([f'wrk -t{WRK_THREADS} -c{WRK_CONNECTIONS} -d{WRK_TIME} '
+                            f'http://{freamework}:{port}/{endpoint} >> /app/results/results.txt'],
+                           shell=True)
+            subprocess.run([f'echo "\n" >> /app/results/results.txt'], shell=True)
+
+        for endpoint in post_endpoints:
+            subprocess.run([f'echo "{endpoint}" >> /app/results/results.txt'], shell=True)
+            subprocess.run([f'wrk -t{WRK_THREADS} -c{WRK_CONNECTIONS} -d{WRK_TIME}  -s ./app/post.lua '
+                            f'http://{freamework}:{port}/{endpoint} >> /app/results'
+                            f'/results.txt'],
                            shell=True)
             subprocess.run([f'echo "\n" >> /app/results/results.txt'], shell=True)
         subprocess.run([f'echo "\n" >> /app/results/results.txt'], shell=True)
